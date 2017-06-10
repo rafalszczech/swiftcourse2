3ed201f15ba7926acac62513c36834f89bd297a5
Rafal Szczech <rafalszczech@MacBook-Pro-Rafal-2.local>
2017-06-10 19:13:29 +0200

    Initial Commit

---
+++ SwiftCourse2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCourse2
+//
+//  Created by Rafal Szczech on 10.06.2017.
+//  Copyright © 2017 Rafal Szczech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
